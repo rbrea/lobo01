838bc8869595c9b42df0210e9d11e46018b3091e
rbrea <rbrea@despegar.com>
2016-10-18 01:08:52 -0300

    SGPD marca de dev total agregada

--- pagodiario-domain/src/main/resources/sql/20161003.sql
+++ pagodiario-domain/src/main/resources/sql/20161003.sql
@@ -5,3 +5,5 @@ alter table product_aud add column PRODUCT_TYPE varchar(100);
 
 alter table product add column STOCK_COUNT int not null default 0;
 alter table product_aud add column STOCK_COUNT int;
+
+alter table bill add column DEV_TOTAL_MARK bit;
